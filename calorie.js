3c4c2fcc20f0b3e6d29e2e7684f8eb8026438095
Eveline <evelinembimbi@gmail.com>
2022-12-07 11:56:23 +0100

    résolutions du premier jour

---
+++ calorie.js
@@ -0,0 +1,25 @@
+const { readFileSync } = require("fs");
+
+const input = readFileSync("in.txt", "utf8");
+const inputArray = input.split("\n");
+let mostCalories = 0;
+let currentCount = 0;
+const totals = [];
+inputArray.forEach((line) => {
+  totals.push(currentCount);
+  if (line == "") {
+    if (currentCount > mostCalories) {
+      mostCalories = currentCount;
+    }
+    currentCount = 0;
+  } else {
+    currentCount += Number(line);
+  }
+});
+const total = totals
+  .sort((a, b) => a - b)
+  .slice(-3)
+  .reduce((a, b) => a + b);
+
+console.log(mostCalories);
+console.log(total);
